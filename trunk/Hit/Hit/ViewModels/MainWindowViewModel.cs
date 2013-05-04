e961c4b17b6ffb807357b480b290481385cef8c3
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-04 08:07:36 +0000

    Add Commands to buttons

--- trunk/Hit/Hit/ViewModels/MainWindowViewModel.cs
+++ trunk/Hit/Hit/ViewModels/MainWindowViewModel.cs
@@ -2,12 +2,135 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 using UseAbilities.MVVM.Base;
+using UseAbilities.MVVM.Command;
 
 namespace Hit.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private ICommand _addABBYYMailHitCommand;
+        public ICommand AddABBYYMailHitCommand
+        {
+            get
+            {
+                return _addABBYYMailHitCommand ?? (_addABBYYMailHitCommand = new RelayCommand(param => OnAddABBYYMailHitCommand(), null));
+            }
+        }
 
+        private void OnAddABBYYMailHitCommand()
+        {
+
+        }
+
+
+        private ICommand _addABBYYCallHitCommand;
+        public ICommand AddABBYYCallHitCommand
+        {
+            get
+            {
+                return _addABBYYCallHitCommand ?? (_addABBYYCallHitCommand = new RelayCommand(param => OnAddABBYYCallHitCommand(), null));
+            }
+        }
+
+        private void OnAddABBYYCallHitCommand()
+        {
+
+        }
+
+
+
+        private ICommand _addFILENETMailHitCommand;
+        public ICommand AddFILENETMailHitCommand
+        {
+            get
+            {
+                return _addFILENETMailHitCommand ?? (_addFILENETMailHitCommand = new RelayCommand(param => OnAddFILENETMailHitCommand(), null));
+            }
+        }
+
+        private void OnAddFILENETMailHitCommand()
+        {
+
+        }
+
+
+        private ICommand _addFILENETCallHitCommand;
+        public ICommand AddFILENETCallHitCommand
+        {
+            get
+            {
+                return _addFILENETCallHitCommand ?? (_addFILENETCallHitCommand = new RelayCommand(param => OnAddFILENETCallHitCommand(), null));
+            }
+        }
+
+        private void OnAddFILENETCallHitCommand()
+        {
+
+        }
+
+
+
+
+        private ICommand _addSAPMailHitCommand;
+        public ICommand AddSAPMailHitCommand
+        {
+            get
+            {
+                return _addSAPMailHitCommand ?? (_addSAPMailHitCommand = new RelayCommand(param => OnAddSAPMailHitCommand(), null));
+            }
+        }
+
+        private void OnAddSAPMailHitCommand()
+        {
+
+        }
+
+
+        private ICommand _addSAPCallHitCommand;
+        public ICommand AddSAPCallHitCommand
+        {
+            get
+            {
+                return _addSAPCallHitCommand ?? (_addSAPCallHitCommand = new RelayCommand(param => OnAddSAPCallHitCommand(), null));
+            }
+        }
+
+        private void OnAddSAPCallHitCommand()
+        {
+
+        }
+
+
+
+        private ICommand _addEnvironmentMailHitCommand;
+        public ICommand AddEnvironmentMailHitCommand
+        {
+            get
+            {
+                return _addEnvironmentMailHitCommand ?? (_addEnvironmentMailHitCommand = new RelayCommand(param => OnAddEnvironmentMailHitCommand(), null));
+            }
+        }
+
+        private void OnAddEnvironmentMailHitCommand()
+        {
+
+        }
+
+
+        private ICommand _addEnvironmentCallHitCommand;
+        public ICommand AddEnvironmentCallHitCommand
+        {
+            get
+            {
+                return _addEnvironmentCallHitCommand ?? (_addEnvironmentCallHitCommand = new RelayCommand(param => OnAddEnvironmentCallHitCommand(), null));
+            }
+        }
+
+        private void OnAddEnvironmentCallHitCommand()
+        {
+
+        }
     }
 }

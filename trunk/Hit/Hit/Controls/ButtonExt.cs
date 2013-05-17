e2740488ae48c58ce7a0d155c2fc59146b6a7021
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-17 17:07:10 +0000

    Add ButtonExt with Beautiful +1 effect

---
+++ trunk/Hit/Hit/Controls/ButtonExt.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Hit.Controls
+{
+    public class ButtonExt : Button
+    {
+        #region DependencyProperty AdditionalContent
+
+        public static readonly DependencyProperty AdditionalContentProperty =
+            DependencyProperty.Register("AdditionalContent",
+                                        typeof(string),
+                                        typeof(ButtonExt),
+                                        new UIPropertyMetadata(null, AdditionalContentChanged)
+                                        );
+
+        public string AdditionalContent
+        {
+            get
+            {
+                return (string)GetValue(AdditionalContentProperty);
+            }
+            set
+            {
+                SetValue(AdditionalContentProperty, value);
+            }
+        }
+
+        private static void AdditionalContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //
+        }
+
+        #endregion
+    }
+}

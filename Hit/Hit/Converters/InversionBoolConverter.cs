a2c7b3dfca7656de8b731c5311762ae8f0b6d7eb
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-17 15:46:30 +0000

    Fix Button Style  Ainmation

---
+++ Hit/Hit/Converters/InversionBoolConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using UseAbilities.WPF.Converters.Base;
+
+namespace Hit.Converters
+{
+    public class InversionBoolConverter : ConvertorBase<InversionBoolConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !((bool) value);
+        }
+    }
+}

e431aaa16b4d4f76af17893a422d5228d87a9813
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-19 12:30:00 +0000

    Add feature displaing on Buttons count of request by type

---
+++ Hit/Hit/Models/RequestTheme.cs
@@ -0,0 +1,10 @@
+﻿namespace Hit.Models
+{
+    public struct RequestTheme
+    {
+        public static readonly int ABBYY = 1;
+        public static readonly int FILENET = 2;
+        public static readonly int SAP = 3;
+        public static readonly int Environment = 4;
+    }
+}
---
+++ Hit/Hit/Models/RequestType.cs
@@ -0,0 +1,8 @@
+﻿namespace Hit.Models
+{
+    public struct RequestType
+    {
+        public static readonly int Email = 1;
+        public static readonly int Call = 2;
+    }
+}

e441bdb0bc284ea0c1d605864e6b258d5127da41
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-21 10:32:21 +0000

    Add DBStore class

---
+++ trunk/Hit/Hit/Stores/Base/DBStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hit.Stores.Base
+{
+    public class DBStore : StoreBase<string>
+    {
+
+    }
+}

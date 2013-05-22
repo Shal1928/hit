64c2611fc3157e77b1b6284dee36e56d075f1e72
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-22 10:27:03 +0000

    Add DBStore members

--- Hit/Hit/Stores/Base/DBStore.cs
+++ Hit/Hit/Stores/Base/DBStore.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Objects;
 using System.Linq;
 using System.Text;
+using Hit.Models.Base;
 
 namespace Hit.Stores.Base
 {
-    public class DBStore : StoreBase<string>
+    public abstract class DBStore<T> : StoreBase<T> where T: DataObject
     {
+        protected DBStore(string entitySetName)
+        {
+            EntitySetName = entitySetName;
+        }
 
+        protected readonly string EntitySetName;
+
+        protected abstract ObjectContext ObjectContext
+        {
+            get;
+        }
+
+        protected abstract ObjectSet<T> GetObjectSet();
+
+        protected virtual void OnApplyContextChanges()
+        {
+            ObjectContext.SaveChanges();
+        }
+
+        protected virtual T OnLoadObject(int key)
+        {
+            return (T)ObjectContext.GetObjectByKey(CreateEntityKey(key));
+        }
+
+        protected EntityKey CreateEntityKey(int key)
+        {
+            return new EntityKey(EntitySetName, EnumerableEx.Return(new KeyValuePair<string, object>("ID", key)));
+        }
     }
 }

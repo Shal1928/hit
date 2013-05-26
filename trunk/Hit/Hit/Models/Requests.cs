d2137fac5be27a5f598f8cb5a6a05d6b8ffe7e89
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-26 13:16:08 +0000

    Add DBstores parts

---
+++ trunk/Hit/Hit/Models/Requests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Objects.DataClasses;
+using System.Runtime.Serialization;
+using Hit.Models.Base;
+
+namespace Hit.Models
+{
+    [EdmEntityType(NamespaceName = "HitsModel", Name = "Requests")]
+    [Serializable()]
+    [DataContract(IsReference = true)]
+    public class Requests : UniqueObject
+    {
+        [DataMember]
+        public int RequestThemesId
+        {
+            get; 
+            set;
+        }
+
+        [DataMember]
+        public int RequestTypesId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime RequestDate
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ trunk/Hit/Hit/Stores/HitsObjectContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.EntityClient;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using Hit.Models;
+
+namespace Hit.Stores
+{
+    public class HitsObjectContext : ObjectContext
+    {
+        private HitsObjectContext() : base("name=HitsEntities", "HitsEntities")
+        {
+            ContextOptions.ProxyCreationEnabled = false;
+
+            //HitsEntities.Requests
+            try
+            {
+                _requests = CreateObjectSet<Requests>("Requests");
+            }
+            catch (Exception e)
+            {
+                
+                throw;
+            }
+            
+        }
+
+        private static readonly HitsObjectContext Instance = new HitsObjectContext();
+        public static HitsObjectContext GetInstance()
+        {
+            return Instance;
+        }
+
+        private ObjectSet<Requests> _requests = null;
+        public ObjectSet<Requests> Requests
+        {
+            get
+            {
+                return _requests;
+            }
+        }
+    }
+}

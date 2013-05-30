a967548e468b98eb473e6d9c8f83e79ec918a090
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-30 06:23:57 +0000

    Make Expander expanding with mouse enter delay

---
+++ trunk/Hit/Hit/Behaviors/ExpanderMouseOverBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using System.Windows.Threading;
+
+namespace Hit.Behaviors
+{
+    public class ExpanderMouseOverBehavior : Behavior<Expander>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseEnter += OnMouseOverDelay;
+            AssociatedObject.MouseLeave += OnMouseLeave;
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.MouseEnter -= OnMouseOverDelay;
+        }
+
+        private static DispatcherTimer _dispatcherTimer;
+
+        private void OnMouseOverDelay(object sender, EventArgs e)
+        {
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += MosueTickCheck;
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 400);
+            _dispatcherTimer.Start();
+        }
+
+        private void OnMouseLeave(object sender, EventArgs e)
+        {
+            AssociatedObject.IsExpanded = false;
+            _dispatcherTimer.Stop();
+            _dispatcherTimer = null;
+        }
+
+        private void MosueTickCheck(object sender, EventArgs e)
+        {
+            AssociatedObject.IsExpanded = true;
+        }
+    }
+}

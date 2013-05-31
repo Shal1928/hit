b4d23817401c5717b8471abae31bba3c177fb7f8
shal1928 <shal1928@44e1a901-c239-b5ab-24e8-cf36225d299d>
2013-05-31 10:35:01 +0000

    Add WeekChart

---
+++ Hit/Hit/Controls/WeekChart.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hit.Controls
+{
+    /// <summary>
+    /// Interaction logic for WeekChart.xaml
+    /// </summary>
+    public partial class WeekChart : UserControl
+    {
+        public WeekChart()
+        {
+            InitializeComponent();
+        }
+
+        #region DependencyProperty SelectedColumnIndex
+
+        public static readonly DependencyProperty MondayHitCount =
+            DependencyProperty.Register("SelectedColumnIndex",
+                                        typeof(int),
+                                        typeof(ColumnHeaderFilterBehavior),
+                                        new UIPropertyMetadata(0, SelectedColumnIndexChanged)
+                                        );
+
+        public int SelectedColumnIndex
+        {
+            get
+            {
+                return (int)GetValue(SelectedColumnIndexProperty);
+            }
+            set
+            {
+                SetValue(SelectedColumnIndexProperty, value);
+            }
+        }
+
+        private static void SelectedColumnIndexChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //
+        }
+
+        #endregion
+    }
+}
